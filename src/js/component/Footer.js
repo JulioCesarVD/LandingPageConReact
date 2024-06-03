0d644989681d1b591de8e2274b71435089fd3eb0
Julio Cesar Vasquez <103007270+JulioCesarVD@users.noreply.github.com>
2024-06-03 10:40:16 +0000

    add footer

---
+++ src/js/component/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light mt-4 p-4 d-flex justify-content-center">
+    <p className="text-dark">Copyright © Your website 2024</p>
+</nav>
+
+  );
+};
+
+export default Footer;
