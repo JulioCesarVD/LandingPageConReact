16b663572b4de2c9a331f9a03ae26954f5108318
Julio Cesar Vasquez <103007270+JulioCesarVD@users.noreply.github.com>
2024-06-15 16:45:39 +0000

    add price

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Card = ({ image, title, text, buttonText }) => {
+
+const Card = ({ image, title, text,price, buttonText }) => {
   return (
     <div className="card mt-5" style={{ width: '15rem' }}>
       <img className="card-img-top" src={image} alt="Card image cap" />
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
         <p className="card-text">{text}</p>
+        <h6 className="card-text">{price}</h6>
         <a href="#" className="btn btn-primary">{buttonText}</a>
       </div>
     </div>
@@ -18,6 +20,7 @@ Card.propTypes = {
   image: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
   buttonText: PropTypes.string.isRequired,
 };
 
