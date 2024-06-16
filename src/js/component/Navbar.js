4ee76b8bca8b3a2d1ddab7eefe014959243ce468
Julio Cesar Vasquez <103007270+JulioCesarVD@users.noreply.github.com>
2024-06-16 16:49:57 +0000

    bootstrap styles for containers

--- src/js/component/Navbar.js
+++ src/js/component/Navbar.js
@@ -1,24 +1,32 @@
 import React from 'react';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark ">
-      <a className="navbar-brand ms-5 font-monospace" href="#">Elite Cars</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <a className="nav-link" href="#">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Features</a>
-          </li>
-          <li className="nav-item me-5">
-            <a className="nav-link" href="#">Pricing</a>
-          </li>
-        </ul>
+    <nav className="navbar navbar-expand-lg navbar-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand ms-5 font-monospace" href="#">Elite Cars</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-between" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Features</a>
+            </li>
+            <li className="nav-item me-5">
+              <a className="nav-link" href="#">Pricing</a>
+            </li>
+          </ul>
+          <ul className="navbar-nav me-5">
+            <li className="nav-item">
+              <a className="nav-link " href="#"><i className="fas fa-shopping-cart fs-4"></i></a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
