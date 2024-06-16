024d6147af86a79106fc9d6d0d2d654480b3f652
Julio Cesar Vasquez <103007270+JulioCesarVD@users.noreply.github.com>
2024-06-16 16:49:37 +0000

    bootstrap styles for containers

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -5,16 +5,17 @@ import PropTypes from 'prop-types';
 const Card = ({ image, title, text,price, buttonText }) => {
   return (
     <div className="card mt-5">
-    <img className="card-img-top" src={image} alt="Card image cap" />
-    <div className="card-body">
-      <h5 className="card-title">{title}</h5>
-      <p className="card-text">{text}</p>
-      <h6 className="card-text">{price}</h6>
-      <a href="#" className="btn btn-primary">{buttonText}</a>
+      <img className="card-img-top" src={image} alt="Card image cap" />
+      <div className="card-body d-flex flex-column">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{text}</p>
+        <h6 className="card-text mt-auto align-self-start">{price}</h6>
+        <a href="#" className="btn btn-primary mt-auto align-self-start">{buttonText}</a>
+      </div>
     </div>
-  </div>
   );
 };
+ 
 
 Card.propTypes = {
   image: PropTypes.string.isRequired,
