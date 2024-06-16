e8e67811dd16c7d04049fcbbb091de1f64dfce8f
Julio Cesar Vasquez <103007270+JulioCesarVD@users.noreply.github.com>
2024-06-16 15:57:21 +0000

    added new styles for the card section

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -4,15 +4,15 @@ import PropTypes from 'prop-types';
 
 const Card = ({ image, title, text,price, buttonText }) => {
   return (
-    <div className="card mt-5" style={{ width: '15rem' }}>
-      <img className="card-img-top" src={image} alt="Card image cap" />
-      <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">{text}</p>
-        <h6 className="card-text">{price}</h6>
-        <a href="#" className="btn btn-primary">{buttonText}</a>
-      </div>
+    <div className="card mt-5">
+    <img className="card-img-top" src={image} alt="Card image cap" />
+    <div className="card-body">
+      <h5 className="card-title">{title}</h5>
+      <p className="card-text">{text}</p>
+      <h6 className="card-text">{price}</h6>
+      <a href="#" className="btn btn-primary">{buttonText}</a>
     </div>
+  </div>
   );
 };
 
